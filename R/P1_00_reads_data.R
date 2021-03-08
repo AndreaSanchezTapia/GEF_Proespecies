a2a115296b9ff969b6ca50e8c9995b955659a31b
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-03-08 13:31:40 -0800

    formata os binomios da lista oficial de Sao Paulo

--- R/P1_00_reads_data.R
+++ R/P1_00_reads_data.R
@@ -4,33 +4,27 @@ library(dplyr)
 library(tidyr)
 library(flora)
 library(readr)
-# remotes::install_github("liibre/Rocc")
+#remotes::install_github("liibre/Rocc")
 library(Rocc)
 library(stringr)
+library(textclean)
+library(janitor)
 
 # campos do produto 1 ----
 campos_p1 <- c("grupo", "familia", "genero", "epiteto_especifico", "especie", "sinonimia", "fonte_sinonimia", "cat_ameaca_iucn", "cat_ameaca_br", "cat_ameaca_cncflora", "cat_ameaca_sp")
 
-#1. ler IUCN_BR_SP ----
+# 1. ler IUCN_BR_SP ----
 IUCN_CNCFlora_BR_SP <- read_xlsx("data/DADOS/Flora_EstadoSP_Listas_IUCN_CNCFlora_BR_SP.xlsx")
 
 # rename columns ----
-format_names <- function(x) {
-  x <- tolower(replace_non_ascii(x))
-  x <- gsub(x = x, pattern = " ",replacement = "_")
-  }
-IUCN_CNCFlora_BR_SP <- rename_with(
-  .data = IUCN_CNCFlora_BR_SP,
-  .fn = format_names,
-  .cols = everything()
-)
+IUCN_CNCFlora_BR_SP <- clean_names(IUCN_CNCFlora_BR_SP)
 names(IUCN_CNCFlora_BR_SP)# tem campos demais
 
 # manter categorias anteriores ----
 #vou revisar so no final
 IUCN_CNCFlora_BR_SP <- IUCN_CNCFlora_BR_SP %>%
-  rename(cat_ameaca_br_old = `br_oficial_(publicado_em_portaria_pelo_mma)`,
-         cat_ameaca_cncflora_old = `cncflora_atual_(quando_diferente_da_coluna_anterior)`,
+  rename(cat_ameaca_br_old = br_oficial_publicado_em_portaria_pelo_mma,
+         cat_ameaca_cncflora_old = cnc_flora_atual_quando_diferente_da_coluna_anterior,
          cat_ameaca_sp_old = sp)
 # checa se tem outros clados
 IUCN_CNCFlora_BR_SP %>% count(reino)#only plants
@@ -47,32 +41,37 @@ write_csv(IUCN_CNCFlora_BR_SP, "data/dados_formatados/IUCN_CNCFlora_BR_SP_format
 
 ## 2. le Sao Paulo ----
 SP <- read_xlsx("data/DADOS/Flora_Ameacada_SPOficial.xlsx")
-head(SP)
-SP <- rename_with(
-  .data = SP,
-  .fn = format_names,
-  .cols = everything()
-)
-
+SP <- janitor::clean_names(SP)
 SP$familia <- stringr::str_to_sentence(SP$familia)
 SP <- SP %>% rename(especie_autor = especie)
 SP <- SP %>% rename(cat_ameaca_sp = status_de_conservacao)
 SP
-#removes authors
-#SP$especie <- data.frame(especie = sapply(X = SP$especie_autor,
- #                               FUN = remove.authors))
-remove.authors2 <- Vectorize(remove.authors)
-SP <- SP %>% mutate(especie = remove.authors2(especie_autor))
 
-library(Rocc)
-remotes::install_github("liibre/Rocc", force = T)
+#alem de author removal tem que ver o resto do string
+rocc_check1 <- check_string(SP$especie_autor) #com autor
+rocc_check2 <- check_string(rocc_check1$species)
+rocc_check <- cbind(rocc_check1, rocc_check2)
+rocc_check <- janitor::clean_names(rocc_check)
 
-#checar essa remocao
-noauth <- SP %>% filter(especie_autor == especie) %>% pull(especie)
-sum(SP$especie_autor %in% noauth)
-SP[SP$especie_autor %in% noauth, "especie"] <- "Lysimachia buxifolia"
-SP %>% count(especie_autor != especie)
-rocc_check <- Rocc::check_string(SP$especie)
+names(rocc_check)
+count(rocc_check, species_status, species_status_2)
+
+rocc_check %>% filter(species_status != "name_w_authors") %>% View()
+#usa verbatim
+rocc_check <- rocc_check %>%
+  mutate(especie = case_when(
+  species_status %in% c("subspecies", "variety", "forma") ~ verbatim_species,
+  species_status_2 %in% c("possibly_ok") & species_status %in% c("name_w_authors") ~ species_2))
+
+# modificar a mao os que faltam
+l <- length(rocc_check$especie[rocc_check$species_status != "name_w_authors"])
+for (i in seq_along(1:l)) {
+rocc_check$especie[rocc_check$species_status != "name_w_authors"][i] <- remove.authors(rocc_check$especie[rocc_check$species_status != "name_w_authors"][i])
+}
+SP$especie <- rocc_check$especie
+# seleciona os nomes
+SP <- SP %>% select(familia, especie_autor, especie, cat_ameaca_sp)
+write_csv(SP, "data/dados_formatados/SP_format.csv")
 #blz
 
 
