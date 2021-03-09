f1c12af4581401b126495e744b8442ddbf7fe88e
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-03-08 21:24:24 -0800

    janitorial

--- R/00_google_drive_get_data.R
+++ R/00_google_drive_get_data.R
@@ -4,4 +4,4 @@ all_files <- drive_ls(gdrive_folder)
 dados <- drive_ls(all_files[all_files$name == "DADOS",])
 dados_get <- drive_get(path = all_files[all_files$name == "DADOS",]$id)
 SP_oficial <- dados %>% drive_get()
-R.Version()
+
