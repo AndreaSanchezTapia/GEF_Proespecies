5b0411e4e164d205dd9dc147cf9c98b1f98337c6
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-03-10 20:44:17 -0800

    add and format mpoSP data

---
+++ R/P1_06_format_mpoSP.R
@@ -0,0 +1,15 @@
+library(readxl)
+library(janitor)
+library(readr)
+
+mpoSP <- read_xlsx("data/dados_crus/Lista Ameaçadas Município SP.xlsx", skip = 2)
+mpoSP <- janitor::clean_names(mpoSP)
+names(mpoSP)
+mpoSP_format <- mpoSP %>%
+  select(-x1) %>%
+  rename(
+    especie_original = nome_cientifico_sem_autor,
+    especie_autor = nome_cientifico_completo,
+    cat_ameaca_mpoSP = categoria_do_risco_de_exticao)
+mpoSP_format
+write_csv(mpoSP_format, "data/dados_formatados/mpoSP_format.csv")
