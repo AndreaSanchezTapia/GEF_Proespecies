9e91c5d7efc0fa3e90c561f866765629be87c5c2
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-03-10 19:19:42 -0800

    update data_cncflora

--- R/P1_05_format_cncflora.R
+++ R/P1_05_format_cncflora.R
@@ -1,18 +1,40 @@
 #formats cncfloraweb
-
+library(readxl)
 #cncflora pagina web ler e formatar
-cncflora_flora <- read_csv("./data/dados_crus/status_cncflora.csv")
-sum(is.na(cncflora_flora$threat.status))
+#cncflora_flora <- read_csv("./data/dados_crus/status_cncflora.csv")
+#agora usamos o excel oficial
+cncflora <- read_xlsx("data/dados_crus/Lista de avaliadas CNCFlora até outubro de 2020.xlsx",
+                      sheet = 4)
+names(cncflora)
 
-#rename
-cncflora_format <- cncflora_flora %>%
-  rename(especie = search.str,
-         cat_ameaca_cncfloraweb = threat.status) %>%
-  select(cat_ameaca_cncfloraweb, especie)
+#corrige cattleya labiata 0 e 1
+cncflora$Selecionar_ultima_avaliacao[cncflora$`Taxa avaliado` == "Cattleya labiata" & cncflora$Ano == 2013] <- 0
+
+cncflora_format <- cncflora %>% rename(
+  especie_original = `Taxa avaliado`,
+  cat_ameaca_cncflora = Categoria,
+  avaliacao = Selecionar_ultima_avaliacao
+) %>% select(especie_original, cat_ameaca_cncflora, avaliacao) %>%
+  distinct()
+count(cncflora_format, avaliacao)
+count(cncflora_format, especie_original, avaliacao) %>% arrange(desc(n))
+
+#checando, tudo bem com a correcao
+reavaliados <- cncflora_format %>% filter(avaliacao == 0) %>% pull(especie_original)
+cncflora_format %>% filter(especie_original %in% reavaliados) %>% count(especie_original, avaliacao) %>% View()
+
+
+#separar em duas colunas de ameaca
+library(tidyr)
+cncflora_format_dual <- pivot_wider(data = cncflora_format,
+                                    id_cols = "especie_original",
+                                      names_from = "avaliacao",
+                                    values_from = "cat_ameaca_cncflora", names_prefix = "cat_ameaca_cncflora_")
+cncflora_format_dual
+cncflora_format_dual %>% filter(!is.na(cat_ameaca_cncflora_0))
 #save
 output <- "data/dados_formatados"
-write_csv(cncflora_format, fs::path(output, "status_cncflora_format.csv"))
-
+write_csv(cncflora_format_dual, fs::path(output, "cncflora_format.csv"))
 # checa quais dessas especies estao em sao paulo
 
 cncfloraweb_flora <- get.taxa(cncflora_format$especie,
