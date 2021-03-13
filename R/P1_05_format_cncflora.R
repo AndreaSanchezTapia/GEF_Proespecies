f96224ae92b44fb4b6a0a10987149abec1f4fda3
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-03-13 14:37:19 -0800

    adds fonte to cncflora

--- R/P1_05_format_cncflora.R
+++ R/P1_05_format_cncflora.R
@@ -1,22 +1,29 @@
 #formats cncfloraweb
 library(readxl)
 devtools::load_all("../../R_packages/flora/")
+
 #cncflora pagina web ler e formatar
-#cncflora_flora <- read_csv("./data/dados_crus/status_cncflora.csv")
+cncflora_web <- read_csv("./data/dados_crus/status_cncflora.csv") %>% select(-1)
+cncflora_web <- janitor::clean_names(cncflora_web) %>%
+  mutate(fonte = "cncflora_web")
 #agora usamos o excel oficial
-cncflora <- read_xlsx("data/dados_crus/Lista de avaliadas CNCFlora até outubro de 2020.xlsx",
+cncflora_out <- read_xlsx("data/dados_crus/Lista de avaliadas CNCFlora até outubro de 2020.xlsx",
                       sheet = 4)
-names(cncflora)
+cncflora_out <- janitor::clean_names(cncflora_out) %>%
+  mutate(fonte = "cncflora_out")
+cncflora_ckan <- read_csv("data/dados_crus/avaliacao-do-risco-de-extincao-de-arvore-nativas-do-brasil-2018-junho-2020.csv")
+cncflora_ckan <- janitor::clean_names(cncflora_ckan) %>%
+  mutate(fonte = "cncflora_ckan")
 
 #corrige cattleya labiata 0 e 1
-cncflora$`Taxa avaliado`[cncflora$`Taxa avaliado` == "Cattleya labiata" & cncflora$Ano == 2013]
-cncflora$`Taxa avaliado`[cncflora$`Taxa avaliado` == "Cattleya labiata" & cncflora$Ano == 2018] <- "Cattleya lobata"
+cncflora_out$taxa_avaliado[cncflora_out$taxa_avaliado == "Cattleya labiata" & cncflora_out$ano == 2013]
+cncflora_out$taxa_avaliado[cncflora_out$taxa_avaliado == "Cattleya labiata" & cncflora_out$ano == 2018] <- "Cattleya lobata"
 
-cncflora_format <- cncflora %>% rename(
-  especie_original = `Taxa avaliado`,
-  cat_ameaca_cncflora = Categoria,
-  avaliacao = Selecionar_ultima_avaliacao
-) %>% select(especie_original, cat_ameaca_cncflora, avaliacao) %>%
+cncflora_format <- cncflora_out %>% rename(
+  especie_original = taxa_avaliado,
+  cat_ameaca_cncflora = categoria,
+  avaliacao = selecionar_ultima_avaliacao
+) %>% select(especie_original, cat_ameaca_cncflora, avaliacao, fonte) %>%
   distinct()
 count(cncflora_format, avaliacao)
 count(cncflora_format, especie_original, avaliacao) %>% arrange(desc(n))
@@ -29,7 +36,7 @@ cncflora_format %>% filter(especie_original %in% reavaliados) %>% count(especie_
 #separar em duas colunas de ameaca
 library(tidyr)
 cncflora_format_dual <- pivot_wider(data = cncflora_format,
-                                    id_cols = "especie_original",
+                                    id_cols = c("especie_original", "fonte"),
                                     names_from = "avaliacao",
                                     values_from = "cat_ameaca_cncflora",
                                     names_prefix = "cat_ameaca_cncflora_")
@@ -37,7 +44,7 @@ cncflora_format_dual
 cncflora_format_dual %>% filter(!is.na(cat_ameaca_cncflora_0))
 #save
 output <- "data/dados_formatados"
-write_csv(cncflora_format_dual, fs::path(output, "cncflora_format.csv"))
+write_csv(cncflora_format_dual, fs::path(output, "cncflora_out_format.csv"))
 
 
 # checa quais dessas especies estao em sao paulo
@@ -56,7 +63,7 @@ cncflora_flora <- get.taxa(cncflora_format_dual$especie_original,
 #rename
 cncflora_flora$especie_original <- cncflora_flora$original.search
 #junta
-cncflora_all <- full_join(cncflora_format_dual, cncflora_flora) #7518
+cncflora_all <- full_join(cncflora_format_dual, cncflora_flora) #7519
 cncflora_all <- distinct(cncflora_all)
 
 #only SP
@@ -71,7 +78,7 @@ cncflora_SP <- cncflora_all %>%
 
 #salva so nome e cat_ameaca
 names(cncflora_SP)
-cncflora_SP <- cncflora_SP %>% select(especie_original, starts_with("cat_ameaca"))
+cncflora_SP <- cncflora_SP %>% select(especie_original, starts_with("cat_ameaca"), fonte)
 write_csv(cncflora_SP, "data/dados_formatados/cncflora_SP_format.csv", na = "s.i.")
 
 
