6d7974572dd59c9ddb7a34d3b50a1dcb7ee1b9de
Andrea Sánchez Tapia <katori@gmail.com>
2021-03-14 14:37:33 -0700

    updated grupo w recent ipt

--- R/P1_08_extrai_grupo.R
+++ R/P1_08_extrai_grupo.R
@@ -1,7 +1,9 @@
 # cria coluna grupo
+library(dplyr)
 library(readr)
 library(flora)
 library(Rocc)
+library(tidyr)
 
 tudo <- read_csv("output/04_tudo_sem formato.csv") %>%
   janitor::clean_names()
@@ -23,21 +25,24 @@ tudo <- tudo %>%
          starts_with("cat_ameaca"),
          fontes)
 
-# ipt antigo da sara
-#unzip("data/dados_crus/ipt/973412d572e928704ba483659033d9cae2e56489.zip", exdir = "data/dados_crus/ipt/" )
-#spprofile <- vroom::vroom("data/dados_crus/ipt/973412d572e928704ba483659033d9cae2e56489/taxon.txt")
-# grupo <- spprofile %>%
-#   select(id, taxonID, scientificName, higherClassification) %>%
-#   separate(higherClassification, into = LETTERS, remove = F) %>%
-#   select(id, taxonID, scientificName, higherClassification, A, B) %>%
-#   janitor::clean_names() %>%
-#   rename(grupo = b)
+spprofile <- vroom::vroom("~/Library/Caches/R/finch/973412d572e928704ba483659033d9cae2e56489/taxon.txt")
+grupo <- spprofile %>%
+  select(id, taxonID, scientificName, higherClassification) %>%
+  separate(higherClassification, into = LETTERS, remove = F) %>%
+  select(id, taxonID, scientificName, higherClassification, A, B) %>%
+  janitor::clean_names() %>%
+  rename(grupo = b)
 
-# wgrupo <- grupo %>%
-#   select(scientific_name, grupo) %>%
-#   filter(scientific_name %in% tudo$scientific_name)
-# write_csv(wgrupo, "data/dados_crus/ipt/grupo.csv")
-grupo <- vroom::vroom("data/dados_crus/ipt/grupo.csv")
+wgrupo <- grupo %>%
+  select(scientific_name, grupo) %>%
+  filter(scientific_name %in% tudo$scientific_name)
+write_csv(wgrupo, "data/dados_crus/ipt/grupo_new.csv")
+
+grupo1 <- vroom::vroom("data/dados_crus/ipt/grupo.csv")
+grupo2 <- vroom::vroom("data/dados_crus/ipt/grupo_new.csv")
+grupo <- full_join(grupo1, grupo2)
+write_csv(grupo, "data/dados_crus/ipt/grupo.csv")
+
 unique(grupo$grupo)
 grupo$grupo[grupo$grupo == "Bri"] <- "Briófitas"
 wgrupo <- grupo %>%
