ec05c25f605b443120b1f45b1a99d439db9d1393
Andrea Sánchez Tapia <katori@gmail.com>
2021-03-14 18:28:03 -0700

    lista anotada

--- R/P1_08_formata_tabela.R
+++ R/P1_08_formata_tabela.R
@@ -4,23 +4,30 @@ library(dplyr)
 library(stringr)
 library(tidyr)
 
-lista <- read_csv("output/04_tudo_sem formato.csv", na = "s.i.")
-flora <- read_csv("output/03_flora_tudo.csv", na = "s.i")
-
+campos_p1
+lista <- read_csv("output/06_lista_com_grupo.csv")
 #renomear
 lista <- lista %>%
   rename(familia = family,
-         especie_autor = scientific.name,
-         busca_FB = original.search,
-         notas = notes)
+         especie_autor = scientific_name,
+         genero = genus,
+         epiteto_especifico = specific_epiteth,
+         epiteto_infraespecifico = infra_epiteth,
+         especie = search_str)
+# conferir taxonrank
+count(lista, taxon_rank)
+#lista %>% filter(taxon_rank != "species") %>% View()
 
-#marco lista buenA
-especies <- lista %>% select(starts_with("especie")) %>% distinct()
-ameaca <- select(lista, starts_with(c("especie", "cat_ameaca")))
+#checa a especies duplicadas
+sp_dupl <- lista %>%
+  count(especie_original) %>%
+  arrange(desc(n)) %>%
+  filter(n == 2) %>% pull(especie_original)
+especies_duplicadas <- filter(lista, especie_original %in% sp_dupl)
+lista <- lista %>% mutate(notas_dupl = if_else(especie_original %in% sp_dupl, "nome duplicado na base original", ""))
 
-especies %>% count(especie_original, especie_autor) %>% arrange(desc(n))
-
-especies_df <- especies %>%
+#lista com os nomes bons
+especies_df <- lista %>%
   group_by(especie_autor) %>%
   mutate(especies_originais = paste(especie_original, collapse = ";")) %>%
   dplyr::select(-especie_original) %>%
@@ -28,16 +35,19 @@ especies_df <- especies %>%
   ungroup()
 names(especies_df)
 
+#
 filter(especies_df, is.na(especie_autor)) %>% write_csv("output/05_not_found_by_FB_correct.csv")
-count(lista , notas, taxon.status)
-filter(lista, notas == "not found") %>% write_csv("output/05_not_found_by_FB.csv")
+count(lista , notes, taxon_status)
+filter(lista, notes == "not found") %>% write_csv("output/05_not_found_by_FB.csv")
 #outros nomes que precisam ser checados
 filter(lista,
-       !notas %in% c("not found", "", "replaced synonym")) %>%
-  filter(taxon.status %in% c(NA)) %>%
+       !notes %in% c("not found", "", "replaced synonym")) %>%
+  filter(taxon_status %in% c(NA)) %>%
   #count(notas, taxon.status)
-  select(especie_original, notas, taxon.status, starts_with("cat")) %>%
+  select(especie_original, notes, taxon_status, starts_with("cat")) %>%
+  arrange(especie_original) %>%
     write_csv("output/05_check_names.csv")
+
 
 # A ESPECIE OCORRE EM SAO PAULO SEGUNDO A FLORA DO BRASIL?
 lista <- lista %>%
@@ -49,34 +59,23 @@ lista <- lista %>%
   mutate(nota_ocorrencia = if_else(!ocorrencia_sp_FB2020,'A espécie não ocorre no Estado de São Paulo segundo a Flora do Brasil', ""))
 
 # algum problema na grafia?
-count(lista, notas, taxon.status)
 lista <- lista %>%
-  mutate(nota_nome = if_else(str_detect(notas, "mispelled"), "Nome original mal escrito", "")) %>%
+  mutate(nota_nome = if_else(str_detect(notes, "mispelled"), "Nome original mal escrito", "")) %>%
 #Na taxonomia?
-  mutate(nota_taxo = if_else(str_detect(notas, "synonym"), "Nome original era sinônimo", "")) %>%
-  mutate(nota_found = if_else(str_detect(notas, "not found"), "Nome não encontrado na FB2020", "")) %>%
-  mutate(nota_checar = if_else(str_detect(notas, "check"), "Nome ainda precisa ser checado", "")) %>%
-  mutate(nota_sinonimo = if_else(str_detect(taxon.status, "synonym"), "Nome ainda é sinônimo, checar", ""))
+  mutate(nota_taxo = if_else(str_detect(notes, "synonym"), "Nome original era sinônimo", "")) %>%
+  mutate(nota_found = if_else(str_detect(notes, "not found"), "Nome não encontrado na FB2020", "")) %>%
+  mutate(nota_checar = if_else(str_detect(notes, "check"), "Nome ainda precisa ser checado", "")) %>%
+  mutate(nota_sinonimo = if_else(str_detect(taxon_status, "synonym"), "Nome ainda é sinônimo, checar", ""))
 
-lista$numero_de_fontes <- lista %>% select(starts_with("cat_ameaca")) %>% mutate_all(.funs =  function(x) if_else(is.na(x), 0, 1)) %>% rowSums(.)
-lista %>% count(numero_de_fontes, ocorrencia_sp_FB2020)
-
-
-#nada do sima bate
-all(lista$cat_ameaca_br == lista$cat_ameaca_br_sima)
-count(lista, cat_ameaca_br, cat_ameaca_br_sima)
-all(lista$cat_ameaca_sp == lista$cat_ameaca_sp_sima)
-all(lista$cat_ameaca_cncflora_1 == lista$cat_ameaca_cncflora_sima)
-all(lista$cat_ameaca_cncflora_0 == lista$cat_ameaca_cncflora_sima)
-all(lista$cat_ameaca_mpoSP == lista$cat_ameaca_sp)
-
-lista <- unite(lista, cat_ameaca_cncflora, cat_ameaca_cncflora_0, cat_ameaca_cncflora_1,remove = F,sep = "|", na.rm = T)
-lista$cat_ameaca_cncflora
-
-lista
-lista %>% filter(str_detect(taxon.status, "synonym")) %>% write_csv("output/05_sinonimos_segundo_FB2020.csv")
-
+# sinonimi
+names(lista)
+lista <- lista %>%
+  mutate(sinonimia = if_else(notes == "replaced synonym", paste("É sinônimo de", especie), "")) %>%
+  mutate(fonte_sinonimia = if_else(notes == "replaced synonym", "FB2020", ""))
+names(lista)
+campos_p1
+#seleciona campos
 lista %>%
-  filter(especie_original == "Stenandrium diphyllum") %>% View()
-lista %>%
-  filter(especie_original == "Vismia martiana") %>% View()
+    select(grupo, familia, especie_original, sinonimia, fonte_sinonimia,
+                 especie, genero, epiteto_especifico, taxon_rank, epiteto_infraespecifico, starts_with("nota"))
+write_csv(lista, "07_lista_anotada")
