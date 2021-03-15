53457042d78739f4ba53e9496e5bfb880913ff8d
Andrea Sánchez Tapia <katori@gmail.com>
2021-03-14 22:45:28 -0700

    apta + nomes

--- R/P1_08_formata_tabela.R
+++ R/P1_08_formata_tabela.R
@@ -3,23 +3,40 @@ library(readr)
 library(dplyr)
 library(stringr)
 library(tidyr)
-
+remotes::install_github("andreasancheztapia/flora")
+library(flora)
 campos_p1
 lista <- read_csv("output/06_lista_com_grupo.csv")
 names(lista)
-lista %>%
-  filter(!is.na(accepted_name)) %>%
-  count(especie_original, scientific_name, accepted_name, taxon_status)
+
+#nombres_especiales <- lista %>%
+ # filter(!is.na(accepted_name)) %>%
+  #select(accepted_name) %>% distinct() %>% pull()
+#names(nombres_especiales) <- nombres_especiales
+nams <- purrr::map(nombres_especiales, ~flora::remove.authors(.x)) %>% unlist()
+
+
+lista %>% select(especie_original, #sin autor,
+                 scientific_name, #autor
+                 accepted_name, #autor
+                 original_search, #no autor
+                 taxon_status) %>% View()
+#lista
+lista <- lista %>%
+  mutate(nome_aceito_correto =
+           case_when(
+             !is.na(accepted_name) ~ accepted_name,
+             is.na(accepted_name) ~ search_str))
 #renomear
 lista <- lista %>%
   rename(familia = family,
          especie_autor = scientific_name,
          genero = genus,
          epiteto_especifico = specific_epiteth,
-         epiteto_infraespecifico = infra_epiteth,
-         nome_aceito_correto = search_str)
+         epiteto_infraespecifico = infra_epiteth
+#         nome_aceito_correto = search_str
+)
 # conferir taxonrank
-count(lista, taxon_rank)
 count(lista, notes)
 #lista %>% filter(taxon_rank != "species") %>% View()
 
@@ -53,6 +70,7 @@ lista <- lista %>%
 
 # algum problema na grafia?
 count(lista, notes)
+count(lista, taxon.status)
 lista <- lista %>%
   mutate(nota_nome = if_else(str_detect(notes, "misspelled"), "Nome original mal escrito", "")) %>%
 #Na taxonomia?
@@ -64,19 +82,19 @@ lista <- lista %>%
 # sinonimi
 names(lista)
 lista <- lista %>%
-  mutate(sinonimia = if_else(notes == "replaced synonym", paste("É sinônimo de", especie), "")) %>%
+  mutate(sinonimia = if_else(notes == "replaced synonym", paste("É sinônimo de",
+                                                                nome_aceito_correto), "")) %>%
   mutate(fonte_sinonimia = if_else(notes == "replaced synonym", "FB2020", ""))
-names(lista)
-campos_p1
+
 
 # apta
-lista %>%
-  mutate(apta)
 #seleciona campos
-View(lista_anotada)
+lista <- lista %>%
+mutate(apta = if_else(ocorrencia_sp_FB2020 == TRUE & taxon_status == "accepted", "apta", ""))
+
 lista_anotada <- lista %>%
     select(grupo, familia, especie_original, sinonimia, fonte_sinonimia,
-                 especie, genero, epiteto_especifico, taxon_rank, epiteto_infraespecifico, starts_with("nota"), starts_with("cat_ameaca"),fontes)
-)
+                 nome_aceito_correto, genero, epiteto_especifico, taxon_rank, epiteto_infraespecifico, starts_with("nota"), starts_with("cat_ameaca"), fontes, apta)
+lista_anotada$apta
 lista_anotada[is.na(lista_anotada)] <- ""
 write_csv(lista_anotada, "output/07_lista_anotada.csv")
