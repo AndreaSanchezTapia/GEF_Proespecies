6a7c5c0334eee88d891226a6f9a84a3a9db6335d
Andrea Sánchez Tapia <katori@gmail.com>
2021-03-15 02:12:16 -0700

    + fontes + apta

--- R/P1_10_formata_cat_ameaca.R
+++ R/P1_10_formata_cat_ameaca.R
@@ -36,27 +36,8 @@ tudo_updated <- tudo_updated %>% select(any_of(names(tudo)[1:10]),
                         starts_with("cat_ameaca"),
                         -ends_with("new"),
                         -ends_with("sima"),
-                        ) %>%
+                        fontes,
+                        apta) %>%
   unite(col = "notas", starts_with("nota"), na.rm = T, sep = "/")
+tudo_updated[is.na(tudo_updated)] <- ""
 write_csv(tudo_updated, "output/08_lista_categoria_atualizada.csv")
-
-
-# atualiza cat_ameaca_br
-# atualiza cat_ameaca_cncflora
-# atualiza cat_ameaca_sp
-# atualiza cat_ameaca_mposp
-
-count(tudo, nota_ocorrencia)
-Não_SP <- tudo %>% filter(str_detect(nota_ocorrencia, "não"))
-Sim_SP <- tudo %>% filter(is.na(nota_ocorrencia))
-Sim_SP %>% count(fontes) %>% View()
-
-
-
-sima <- filter(tudo, str_detect(fontes, "sima")) %>% distinct()
-SP <- filter(tudo, str_detect(fontes, "SP_oficial")) %>% distinct()
-cncflora <- filter(tudo, str_detect(fontes, "cncflora")) %>% distinct()
-portaria <- filter(tudo, str_detect(fontes, "Portaria")) %>% distinct()
-uicn <- filter(tudo, str_detect(fontes, "API")) %>% distinct()
-mpoSP <- filter(tudo, str_detect(fontes, "mpoSP")) %>% distinct()
-CRLAC <- filter(tudo, str_detect(fontes, "CR_Lac")) %>% distinct()
