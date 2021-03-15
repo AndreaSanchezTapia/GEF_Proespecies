416a9d6ff0598bf0389483d34ea372eea6fc04a9
Andrea Sánchez Tapia <katori@gmail.com>
2021-03-14 22:52:53 -0700

    not convinced with taxonomias incertas

--- R/P1_08_formata_tabela.R
+++ R/P1_08_formata_tabela.R
@@ -3,42 +3,27 @@ library(readr)
 library(dplyr)
 library(stringr)
 library(tidyr)
-remotes::install_github("andreasancheztapia/flora")
+#remotes::install_github("andreasancheztapia/flora")
 library(flora)
 campos_p1
 lista <- read_csv("output/06_lista_com_grupo.csv")
 names(lista)
 
-#nombres_especiales <- lista %>%
- # filter(!is.na(accepted_name)) %>%
-  #select(accepted_name) %>% distinct() %>% pull()
-#names(nombres_especiales) <- nombres_especiales
-nams <- purrr::map(nombres_especiales, ~flora::remove.authors(.x)) %>% unlist()
-
-
-lista %>% select(especie_original, #sin autor,
-                 scientific_name, #autor
-                 accepted_name, #autor
-                 original_search, #no autor
-                 taxon_status) %>% View()
-#lista
-lista <- lista %>%
-  mutate(nome_aceito_correto =
-           case_when(
-             !is.na(accepted_name) ~ accepted_name,
-             is.na(accepted_name) ~ search_str))
+# lista <- lista %>%
+#   mutate(nome_aceito_correto =
+#            case_when(
+#              !is.na(accepted_name) ~ accepted_name,
+#              is.na(accepted_name) ~ search_str))
 #renomear
 lista <- lista %>%
   rename(familia = family,
          especie_autor = scientific_name,
          genero = genus,
          epiteto_especifico = specific_epiteth,
-         epiteto_infraespecifico = infra_epiteth
-#         nome_aceito_correto = search_str
+         epiteto_infraespecifico = infra_epiteth,
+         nome_aceito_correto = search_str
 )
 # conferir taxonrank
-count(lista, notes)
-#lista %>% filter(taxon_rank != "species") %>% View()
 
 #checa a especies duplicadas
 sp_dupl <- lista %>%
@@ -69,18 +54,15 @@ lista <- lista %>%
   mutate(nota_ocorrencia = if_else(!ocorrencia_sp_FB2020,'A espécie não ocorre no Estado de São Paulo segundo a Flora do Brasil', ""))
 
 # algum problema na grafia?
-count(lista, notes)
-count(lista, taxon.status)
 lista <- lista %>%
   mutate(nota_nome = if_else(str_detect(notes, "misspelled"), "Nome original mal escrito", "")) %>%
 #Na taxonomia?
   mutate(nota_taxo = if_else(str_detect(notes, "synonym"), "Nome original era sinônimo", "")) %>%
   mutate(nota_found = if_else(str_detect(notes, "not found"), "Nome não encontrado na FB2020", "")) %>%
-  mutate(nota_checar = if_else(str_detect(notes, "check"), "Nome ainda precisa ser checado", "")) %>%
+  mutate(nota_checar = if_else(str_detect(notes, "check"), "Taxonomia incerta", "")) %>%
   mutate(nota_sinonimo = if_else(str_detect(taxon_status, "synonym"), "Nome ainda é sinônimo, checar", ""))
 
 # sinonimi
-names(lista)
 lista <- lista %>%
   mutate(sinonimia = if_else(notes == "replaced synonym", paste("É sinônimo de",
                                                                 nome_aceito_correto), "")) %>%
@@ -98,3 +80,4 @@ lista_anotada <- lista %>%
 lista_anotada$apta
 lista_anotada[is.na(lista_anotada)] <- ""
 write_csv(lista_anotada, "output/07_lista_anotada.csv")
+
