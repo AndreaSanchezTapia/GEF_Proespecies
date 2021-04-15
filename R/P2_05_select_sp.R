9a2f4643ec06bd9c103c1ecd7fa3af2b888869f2
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-04-15 15:55:43 -0700

    resumo anotado

--- R/P2_05_select_sp.R
+++ R/P2_05_select_sp.R
@@ -1,6 +1,6 @@
 library(readxl)
 library(dplyr)
-
+library(readr)
 #le a tabela revisada com coluna "ameacada"
 spp <- read_excel("output/p2/Lista de espécies-geral.xlsx", sheet = 1, na = "NA")
 names(spp)
@@ -10,8 +10,19 @@ spp$nome_aceito_correto[spp$nome_aceito_correto == "Maxillaria meleagris Lindl."
 
 dim(spp)
 
+notas <- spp %>%
+  select(nome_aceito_correto, notas) %>%
+  count(nome_aceito_correto, notas) %>%
+  #arrange(desc(n)) %>% View()
+  group_by(nome_aceito_correto) %>%
+  mutate(notas_all = paste(notas, collapse = "-")) %>%
+  arrange(nome_aceito_correto) %>%
+  ungroup() %>%
+  select(nome_aceito_correto, notas_all) %>%
+  distinct()
+
 #cria "entra" para saber quem vai de fato e organiza por nomes_aceito_correto
-entra <- spp %>%
+  entra <- spp %>%
   select(nome_aceito_correto, `ENTRA OU NAO`) %>%
   count(nome_aceito_correto, `ENTRA OU NAO`) %>%
   group_by(nome_aceito_correto) %>%
@@ -19,7 +30,7 @@ entra <- spp %>%
   arrange(nome_aceito_correto) %>%
   ungroup() %>%
   #count(ameacada)
-  mutate(entra = if_else(ameacada %in% c("AMEACADA", "AMEACADA-NAO"), "entra", "nao entra")) %>%
+  mutate(cat_ameaca = if_else(ameacada %in% c("AMEACADA", "AMEACADA-NAO"), "entra", "nao entra")) %>%
   select(nome_aceito_correto, entra) %>%
   distinct()
 entra
@@ -34,15 +45,29 @@ setdiff(entra$nome_aceito_correto, p2$nome_aceito_correto)
 setdiff(p2$nome_aceito_correto, entra$nome_aceito_correto)
 
 #cruza
+entra
 cruza <- read_csv("output/p2/01_cruzam_shapes.csv") %>%
   rename(nome_aceito_correto = sp)
-resumo <- p2 %>% left_join(cruza)
+
+resumo <- p2 %>% left_join(cruza) %>% left_join(entra) %>% left_join(notas)
 resumo %>% write_csv("output/p2/02_resumo.csv")
-  resumo %>% View()
-resumo %>% count(elegivel_produto_1, splink_t20, gbif_t20) %>% View()
 
+resumo %>% count(entra)
+
+resumo %>%
+  count(elegivel_produto_1 == "examinar",sum > 0,str_detect(notas_all, "ocorre")) %>% View()
+library(stringr)
+resumo %>%
+  mutate(especialista =
+           case_when(
+             sum > 0  & str_detect(notas_all, "ocorre") ~ "FBnao- OCCScruzam",
+             sum == 0 & str_detect(elegivel_produto_1 == "apta") ~ " FBsim- OCCSnaocruzam")
+         ) %>%
+   write_csv("output/p2/03_resumo_anotado.csv")
+
+resumo %
 resumo <- readr::read_csv("output/p2/02_resumo.csv")
-
+resumo$
 
 junto <- left_join(spp, resumo)
 write_csv(junto, "output/p2/P1_atualizado.csv")
