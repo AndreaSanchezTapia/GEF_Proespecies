36a49f3b4a028984ab7a5defae2bcb5b8654441e
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-04-14 17:56:10 -0700

    checa quem cruza

--- R/P2_04_check_occs.R
+++ R/P2_04_check_occs.R
@@ -6,63 +6,61 @@ library(sf)
 library(tmap)
 library(furrr)
 
-#lee la tabla original de sinomimos
-df_sinonimos <- list.files("output/p2/sinonimos/", full.names = T)
-nomes1 <- basename(df_sinonimos)
-nomes <- stringr::str_remove(nomes1, ".csv")
+#lee las tablas de ocurrencias
+occs <- list.files("output/p2/occs/",recursive = T, full.names = T, pattern = ".csv")
+gbif <- occs[stringr::str_detect(occs, "splink", negate = T)]
+splink <- occs[stringr::str_detect(occs, "gbif", negate = T)]
 
-#lee las tablas de specieslink
-splink <- list.files("output/p2/occs/",recursive = T, full.names = T, pattern = ".csv")
-splink <- splink[str_detect(splink, "gbif", negate = T)]
-head(splink)
+nomes_splink <- basename(splink) %>% stringr::str_remove(".csv")
+nomes_gbif   <- basename(gbif)   %>% stringr::str_remove(".csv")
+names(splink) <- nomes_splink
+names(gbif) <- nomes_gbif
 
-#checa NAs nao precisa refazer
-refazer <- splink[stringr::str_detect(splink, "NA.csv")] %>% str_remove("output/p2/occs//") %>%
-  str_remove("/splink/NA.csv")
-refazer
-sp1 <- basename(splink)
-sp1 <- stringr::str_remove(sp1, ".csv")
-
-setdiff(nomes, sp1)
-setdiff(sp1, nomes) #myrcia speldens nao e ameacada
-
-
-plan(multisession, workers = 14)
-df <- furrr::future_map(splink, ~vroom::vroom(.x, guess_max = 1000, delim = ","), .progress = T)
-plan(sequential)
-a <- map(df, ~nrow(.x)) %>% simplify()
-splink[which(a ==0)]
-unlink(splink[which(a ==0)])
-#cruza com os shapes
+#le os shapes
 rbcv <- read_sf("data/dados_crus/RBCV_Limite/RBCV_Limite_datageo_jan2020.shp")
-t20 <- read_sf("data/dados_crus/Municipios_Territorio_20/Municipios_Territorio_20.shp")
+t20  <- read_sf("data/dados_crus/Municipios_Territorio_20/Municipios_Territorio_20.shp")
 
+#checa se cruza
 cruza_shape <- function(tabela, shape) {
-  tt <- st_read(tabela, options = c("X_POSSIBLE_NAMES=decimalLongitude",
-                                     "Y_POSSIBLE_NAMES=decimalLatitude"))
-  if ("sf" %in% class(tt) ) {
-  tt <- st_set_crs(tt, st_crs(shape))
-  joins <- st_join(tt, shape, left = F)#join = st_within)
-  return(if_else(nrow(joins) == 0, FALSE, TRUE))
-  }
+  ss <- basename(tabela) %>% stringr::str_remove(".csv")
+  st <- tryCatch({
+    shp <- st_read(tabela, options = c("X_POSSIBLE_NAMES=decimalLongitude",
+                                "Y_POSSIBLE_NAMES=decimalLatitude"))
+      },
+      error = function(e) {
+        message(paste(ss,"problem"))
+        #message(e)
+        })
+    if ("sf" %in% class(st) ) {
+      st <- st_set_crs(st, st_crs(shape))
+      joins <- st_join(st, shape, left = F)#join = st_within)
+      return(tibble(sp = ss,
+                    inside = if_else(nrow(joins) == 0, FALSE, TRUE)))
+
+    }
 }
 
 
-plan(multisession, workers = 14)
-cruza_t20 <- furrr::future_map(splink, ~cruza_shape(.x, t20), .progress = T)
-cruza_rbcv <- furrr::future_map(splink, ~cruza_shape(.x, rbcv), .progress = T)
-table(unlist(cruza_t20))
-table(unlist(cruza_rbcv))
-warnings()
-tibble(t20 = unlist(cruza_t20), rbcv = unlist(cruza_rbcv)) %>% count(t20, rbcv)
-table(unlist(cruzarbcv))
+plan(multisession, workers = 15)
+#cruza_t20_splink  <- furrr::future_map(splink,
+#                                       ~cruza_shape(.x, t20), .progress = T)
+#cruza_rbcv_splink <- furrr::future_map(splink, ~cruza_shape(.x, rbcv), .progress = T)
+#cruza_rbcv_gbif   <- furrr::future_map(gbif,   ~cruza_shape(.x, rbcv), .progress = T)
+#cruza_t20_gbif   <- furrr::future_map(gbif,   ~cruza_shape(.x, t20), .progress = T)
+
 plan(sequential)
 
+# resume
 
-data(World)
-tm_shape(World, bbox = "Brazil") +
-  tm_borders() +
-    tm_shape(shape) +
-    tm_borders() +
-    tm_shape(tt) +
-    tm_dots()
+a <- cruza_t20_splink %>% bind_rows() %>% rename(splink_t20 = inside)
+b <- cruza_t20_gbif %>% bind_rows() %>% rename(gbif_t20 = inside)
+c <- cruza_rbcv_splink %>% bind_rows() %>% rename(splink_rbcv = inside)
+d <- cruza_rbcv_gbif %>% bind_rows() %>% rename(gbif_rbcv = inside)
+
+#salva quem cruza e quem nao
+full_join(a, b) %>%
+  full_join(c) %>%
+  full_join(d) %>%
+  arrange(sp) %>%
+  mutate(sum = rowSums(across(where(is.logical)), na.rm = T)) %>%
+  write_csv("output/p2/01_cruzam_shapes.csv")
