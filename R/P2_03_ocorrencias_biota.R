aa678835596549b0e7a7e79027e22013f23f4170
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-04-21 12:38:42 -0700

    adiciona CR_Biota

--- R/P2_03_ocorrencias_biota.R
+++ R/P2_03_ocorrencias_biota.R
@@ -6,8 +6,10 @@ library(stringr)
 biota <- finch::dwca_read("https://ipt.biota.org.br/ipt/archive.do?r=biota_occurrences&v=1.3")
 biota$data
 biota <- vroom::vroom(biota$data, guess_max = 132000)
-nrow(biota)
-biota %>% count(scientificName) %>% distinct()
+names(biota)
+biota %>% count(scientificName, genus, specificEpithet) %>% distinct() %>% View()
+
+count(biota, infraspecificEpithet)
 
 
 cats <- readr::read_csv("./output/p2/04_cat_ameaca.csv")
@@ -17,5 +19,15 @@ cat <- left_join(cats, cat_geral)
 CR <- cat %>% mutate(across(-1, ~stringr::str_detect(.x,
   pattern = "CR"), .names = "{.col}_CR")) %>%
   select(ends_with("CR")) %>% rowSums(na.rm = T)
-tibble(nome_aceito_correto = cat$nome_aceito_correto, CR) %>% filter(CR > 0) %>% write_csv("output/p2/13_especies_CR.csv")
+CR <- tibble(nome_aceito_correto = cat$nome_aceito_correto, CR) %>% filter(CR > 0)
+write_csv(CR, "output/p2/13_especies_CR.csv")
+
+
+biota$scientificName[which(biota$scientificName %in% CR$nome_aceito_correto)]
+CR$nome_aceito_correto[which(CR$nome_aceito_correto %in% biota$scientificName) ]
+
+biota_CR <- biota %>% filter(scientificName %in%
+                               CR$nome_aceito_correto)
+dir.create("output/p2/occs/biota/")
+write_csv(biota_CR, "output/p2/occs/biota/biotaCR.csv")
 
