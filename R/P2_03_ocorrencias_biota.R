21ecb4279400787764c60d0f41654bf1ceb16bc6
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-04-21 12:18:16 -0700

    cria tabela de especies CR para filtrar sinbiota

---
+++ R/P2_03_ocorrencias_biota.R
@@ -0,0 +1,21 @@
+#rsthemes::use_theme_auto(lat = 37, lon = -120)
+library(finch)
+library(dplyr)
+library(readr)
+library(stringr)
+biota <- finch::dwca_read("https://ipt.biota.org.br/ipt/archive.do?r=biota_occurrences&v=1.3")
+biota$data
+biota <- vroom::vroom(biota$data, guess_max = 132000)
+nrow(biota)
+biota %>% count(scientificName) %>% distinct()
+
+
+cats <- readr::read_csv("./output/p2/04_cat_ameaca.csv")
+cat_geral <- readr::read_csv("./output/p2/03_entra.csv")
+cat <- left_join(cats, cat_geral)
+
+CR <- cat %>% mutate(across(-1, ~stringr::str_detect(.x,
+  pattern = "CR"), .names = "{.col}_CR")) %>%
+  select(ends_with("CR")) %>% rowSums(na.rm = T)
+tibble(nome_aceito_correto = cat$nome_aceito_correto, CR) %>% filter(CR > 0) %>% write_csv("output/p2/13_especies_CR.csv")
+
