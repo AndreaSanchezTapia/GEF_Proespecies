b3e0b62aec9b77c9f0e565e27de7a8670ba195e6
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2021-04-25 20:29:56 -0700

    junta sampa e no_coord, filtra mpos

--- R/P2_10_procesa_campos.R
+++ R/P2_10_procesa_campos.R
@@ -1,37 +1,40 @@
-camposP2 <- c("ID_registro",
-  "autor_nome_cientifico",
-  "subespecie",
-  "autor_subespecie",
-  "variedade",
-  "autor_variedade",
-  "herbario_origem",
-  "tombo_herbario_origem",
-  "cod_barras_herbario_origem",
-  "registro_duplicado",
-  "Nome_coletor_original",
-  "numero_coletor",
-  "ano_coleta",
-  "mes_coleta",
-  "dia_coleta",
-  "estado",
-  "municipio",
-  "localidade",
-  "long_original",
-  "lat_original",
-  "sist_coord_original",
-  "datum_original",
-  "situacao_coordenada",
-  "altitude",
-  "determinador_original",
-  "ano_determinacao",
-  "mes_determinacao",
-  "dia_determinacao",
-  "tipo_veg_habitat",
-  "fonte_dados") %>% tibble(campo =.)
+library(readr)
+library(dplyr)
+source("R/functions.R")
+sampa <- read_sf2("sampa.csv", column = "XY")
+plot(sampa$Y, sampa$Y.1)
+names(sampa)
+no_col <- read_csv("output/p2/occs/ALL_NO_COLUMN.csv")
+unique(no_col$country)
 
-todos_os_nomes <- simplify(names_raw) %>% unique() %>% sort() %>% tibble(field = .)
-write_csv(camposP2, "output/p2/07_campos_P2.csv")
-write_csv(todos_os_nomes, "output/p2/08_campos_originales.csv")
-###processamento a mao para saber qual selecionar
-sel_fields <- read_csv("output/p2/08_campos_originales.csv")
-sel_fields <- sel_fields %>% filter(select == T)
+no_col <- no_col %>% select(one_of(names(sampa)))
+
+no_col <- no_col %>% mutate(elevation = as.character(elevation),
+                            elevationAccuracy = as.character(elevationAccuracy),
+                            eventDate = as.character(eventDate),
+                            dateIdentified = as.character(dateIdentified),
+                            familyKey = as.character(familyKey),
+                            record_id = as.character(record_id),
+                            year = as.character(year),
+                            month = as.character(month),
+                            day = as.character(day)
+) %>%
+  filter(country %in% c("Brazil", "Brésil") | is.na(country)) %>%
+  filter(stateProvince %in% c("São Paulo", "Sp", "Sao Paulo") | is.na(stateProvince))
+
+sampa_w_no_col <- bind_rows(sampa, no_col)
+
+
+
+tax <- read_csv("output/p2/10_base_P1.csv")
+final_sampa <- left_join(sampa_w_no_col, tax) %>% select(one_of(c(ord_fields, "X", "Y", "t20")))
+dim(final_sampa)
+count(final_sampa, t20, country, stateProvince) %>% View()
+
+t20
+
+
+t20  <- read_sf("data/dados_crus/Municipios_Territorio_20/Municipios_Territorio_20.shp")
+mpos <-textclean::replace_non_ascii(tolower(t20$NM_MUNICIP))
+
+purrr::map(final_sampa$county, sum(str_detect(final_sampa$county, mpos[1]), na.rm = T)
